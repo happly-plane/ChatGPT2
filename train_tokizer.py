a9c1557c2ce75dc1478c5118cf77668b9d354807
Cosima <zhujinhu649@gmail.com>
2024-08-03 21:31:28 +0800

    1

--- train_tokizer.py
+++ train_tokizer.py
@@ -4,10 +4,13 @@ import re
 with open('2.txt','r',encoding='utf-8') as f:
     text = f.read().strip().strip('\n')
 sentences = []
+a = 0
 for d in text:
     d = text.strip()
     if "==" in d or len(d) == 0 or d == "第":
         continue
+        a += 1
+        print(a)
     sentences.append(d)
 with open('3.txt','w',encoding='utf-8') as f:
     f.write("\n".join(sentences))
