a03561764b1ee4d16a081083851c17815ef93af4
Cosima <zhujinhu649@gmail.com>
2024-08-04 00:18:08 +0800

    ``` 实现自定义句号分词训练
    本次更新移除了文本预处理代码，直接使用sentencepiece库进行分词训练。
    通过设置 SentencePieceTrainer 的参数，可以实现对输入文本进行高效的分词学习，以适应特定领域的文本处理需求。
    ```

--- train_tokizer.py
+++ train_tokizer.py
@@ -1,16 +1,31 @@
 import sentencepiece as spm
 import re
 
-with open('2.txt','r',encoding='utf-8') as f:
-    text = f.read().strip().strip('\n')
-sentences = []
-a = 0
-for d in text:
-    d = text.strip()
-    if "==" in d or len(d) == 0 :
-        a += 1
-        print(a)
-        continue
-    sentences.append(d)
-with open('3.txt','w',encoding='utf-8') as f:
-    f.write("\n".join(sentences))
+
+spm.SentencePieceTrainer.Train(
+    input='3.txt',
+    model_prefix='tokenizer',
+    vocab_size=50000,
+    character_coverage=1.0,
+    model_type='bpe',
+    num_threads=6,
+)
+
+
+
+
+
+
+
+
+# zs = re.compile(r"第[0-9]|[1-9][0-9]|1[0-3][0-6][0-6]章")
+# with open('2.txt','r',encoding='utf-8') as f:
+#     text = f.readlines()
+#     for d in text :
+#         if re.match(zs,d):
+#             print(d)
+#         # if re.findall(r"第[0-9]|[1-9][0-9]|1[0-3][0-6][0-6]章",d):
+#         #     print(d)
+
+
+
